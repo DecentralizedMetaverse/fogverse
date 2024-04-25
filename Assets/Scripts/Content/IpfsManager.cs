df8fcf3b5bf9925ddfa90feaf07e14009026ace7
fog <38463346+fog-zs@users.noreply.github.com>
2024-04-25 16:00:57 +0900

    Add ipfs installer

--- Assets/Scripts/Content/IpfsManager.cs
+++ Assets/Scripts/Content/IpfsManager.cs
@@ -1,17 +1,14 @@
 using Cysharp.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Security.Cryptography;
 using DC;
 using UnityEngine;
 using System.IO;
 
 /// <summary>
-/// IPFS‚Æ‚â‚èæ‚è‚ğs‚¤ƒvƒƒOƒ‰ƒ€
+/// IPFSï¿½Æ‚ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½sï¿½ï¿½ï¿½vï¿½ï¿½ï¿½Oï¿½ï¿½ï¿½ï¿½
 /// </summary>
 public class IpfsManager : MonoBehaviour
 {
+    private static readonly string IpfsPath = $"{Application.dataPath}/../ipfs/kubo/ipfs.exe";
     void Start()
     {
         GM.Add<string, UniTask<string>>("IPFSUpload", Upload);
@@ -22,13 +19,13 @@ public class IpfsManager : MonoBehaviour
     /// 
     /// </summary>
     /// <param name="filePath"></param>
-    /// <returns>true: ¬Œ÷</returns>
+    /// <returns>true: ï¿½ï¿½ï¿½ï¿½</returns>
     async UniTask<string> Upload(string filePath)
     {
         //var path = GetPath(fileName);
         if (!GM.Msg<bool>("EncryptFile", filePath)) return "";
-        var ret = await GM.Msg<UniTask<string>>("Exe", "ipfs", $"add \"{filePath}.enc\"");
-        File.Delete($"{filePath}.enc"); // Fileíœ
+        var ret = await GM.Msg<UniTask<string>>("Exe", IpfsPath, $"add \"{filePath}.enc\"");
+        File.Delete($"{filePath}.enc"); // Fileï¿½íœ
 
         GM.Log($"{ret}");
         var words = ret.Split(' ');
@@ -42,13 +39,13 @@ public class IpfsManager : MonoBehaviour
     /// </summary>
     /// <param name="cid"></param>
     /// <param name="filePath"></param>
-    /// <returns>true: ¬Œ÷</returns>
+    /// <returns>true: ï¿½ï¿½ï¿½ï¿½</returns>
     async UniTask<bool> Download(string cid, string filePath)
     {
-        var ret = await GM.Msg<UniTask<string>>("Exe", "ipfs", $"get {cid} -o \"{filePath}.enc\"");
+        var ret = await GM.Msg<UniTask<string>>("Exe", IpfsPath, $"get {cid} -o \"{filePath}.enc\"");
         
         if (!GM.Msg<bool>("DecryptFile", $"{filePath}.enc")) return false;
-        File.Delete($"{filePath}.enc"); // Fileíœ
+        File.Delete($"{filePath}.enc"); // Fileï¿½íœ
 
         return true;
     }   
