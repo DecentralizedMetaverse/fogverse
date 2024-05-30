2b98046d0e96c511adaefe3f12bd70533779bc58
fog <38463346+fog-zs@users.noreply.github.com>
2024-05-30 15:23:59 +0900

    Add command line interface for generating QR codes

--- IoTRegister/qr_code.py
+++ IoTRegister/qr_code.py
@@ -3,3 +3,8 @@ import qrcode
 def create(file_name, txt):
     img = qrcode.make(txt)
     img.save(file_name)
+    
+    
+if __name__ == '__main__':
+    import sys 
+    create(f"{sys.argv[2]}.png", sys.argv[1])
