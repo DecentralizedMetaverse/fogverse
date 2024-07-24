130c26a5b5668ddc5ce2fa54dcd5aea2240ee32e
fog <38463346+fog-zs@users.noreply.github.com>
2024-07-24 12:11:14 +0900

    fix avatar path

--- Assets/Scripts/Core/Constants.cs
+++ Assets/Scripts/Core/Constants.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class Constants
 {
     public static readonly string ContentPath = $"{Application.dataPath}/../content/{{0}}";
-    public static readonly string AvatarPath = $"{Application.dataPath}/../avatar/{{0}}";
+    public static readonly string AvatarPath = $"{Application.dataPath}/../avatar/";
     public static readonly string MetaPath = $"{Application.dataPath}/../meta/{{0}}";
 }
