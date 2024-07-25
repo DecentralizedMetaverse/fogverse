a8ed27c4eae6f71808c5b9839c8d9f1f96f8c3eb
fog <38463346+fog-zs@users.noreply.github.com>
2024-07-25 21:46:06 +0900

    chore: Update .gitignore to include Avatar and Samples directories

---
+++ Assets/Scripts/XR/DesktopVRSwitch.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class DesktopVRSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject desktop;
+    [SerializeField] private GameObject vr;
+
+    private void Start()
+    {
+    }
+}
